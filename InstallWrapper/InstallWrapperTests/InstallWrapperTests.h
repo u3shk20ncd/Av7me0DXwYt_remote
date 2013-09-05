737c459c38fd3c399050f21c66afb0a21d9f5551
u3shk20ncd <triglochin@Salixs-MacBook-Pro.local>
2013-09-04 21:54:36 -0700

    Initial Commit

---
+++ InstallWrapper/InstallWrapperTests/InstallWrapperTests.h
@@ -0,0 +1,13 @@
+//
+//  InstallWrapperTests.h
+//  InstallWrapperTests
+//
+//  Created by Triglochin on 9/4/13.
+//  Copyright (c) 2013 Timblin Services, Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface InstallWrapperTests : SenTestCase
+
+@end
---
+++ InstallWrapper/InstallWrapperTests/InstallWrapperTests.m
@@ -0,0 +1,32 @@
+//
+//  InstallWrapperTests.m
+//  InstallWrapperTests
+//
+//  Created by Triglochin on 9/4/13.
+//  Copyright (c) 2013 Timblin Services, Inc. All rights reserved.
+//
+
+#import "InstallWrapperTests.h"
+
+@implementation InstallWrapperTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in InstallWrapperTests");
+}
+
+@end
